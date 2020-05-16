663ddcdb2a4fb93c1da771beeb127ff11cfa402b
akalish0 <46036888+akalish0@users.noreply.github.com>
2020-05-16 17:29:35 -0500

    Update dataclass.py

--- DeepLearning/dataclass.py
+++ DeepLearning/dataclass.py
@@ -1,5 +1,9 @@
+# Define dataset class to feed into U-Net. 
+
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
+
+# inherits Dataset class from PyTorch
 class FormsDataset(Dataset):
     def __init__(self, images, num_classes: int, transforms=None):
         self.images = images
@@ -9,7 +13,6 @@ class FormsDataset(Dataset):
     def __getitem__(self, idx):
         image = self.images[idx]
         image = image.astype(np.float32)
-        # image = np.expand_dims(image, -1)
         image = image
 
         return image
